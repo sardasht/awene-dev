b214cca854517f4622a75608c77f037b89a39e1f
Ilja Hehenkamp <ilj@hehenka.mp>
2012-09-26 22:03:03 +0000

    Removed files to ignore from index

--- sites/www.awene.com/files/languages/ku_3a792db115e8fadec2a791780e4007de_0.js
+++
@@ -1,1 +0,0 @@
-Drupal.locale = { 'pluralFormula': function($n) { return Number(($n!=1)); }, 'strings': { "Select all rows in this table": "هەموو ڕیزەکان هەڵبژێرە لەم خشتەیەدا", "Alias: @alias": "نازناو: @alias", "No alias": "نازناو نییە", "Translate Text": "دەق وەرگێڕە" } };
